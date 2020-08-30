6f5938ed0b1793aea8dc1550908d6aa862b87d6a
Gaddam Harshavardhan <harshareddy794@gmail.com>
2020-08-30 20:20:34 +0530

    Update and rename PRIME_OR.C to PRIME_OR_NOT.C

--- C programming/Codes/PRIME_OR_NOT.C
+++ C programming/Codes/PRIME_OR_NOT.C
@@ -5,7 +5,11 @@ int i,n,ans;
 clrscr();
 scanf("%d",&n);
 for(i=2;i<n/2;i++){
- if(n%i==0){ans=1;break;}
+ if(n%i==0)
+ {
+  ans=1;
+  break;
+ }
 }
 if(ans==1){printf("Not prime");}
 else{printf("Prime");}
