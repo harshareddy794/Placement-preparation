051f0698aa838e0b57803401b594f529ed505809
harshareddy794 <harshareddy794@gmail.com>
2020-11-03 22:39:03 +0530

    Added metod 2

---
+++ Ds and algo/Array/Leader in array/method2.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+void leader(){
+	int arr [] = {7,10,4,10,6,5,2},i,j,n=7;
+	int curr_lead= arr[n-1];
+	std::cout<<curr_lead;
+	for(i=n-2;i>=0;i--){
+		if(curr_lead<arr[i]){
+			std::cout<<arr[i];
+			std::cout<<" ";
+			curr_lead = arr[i];
+		}
+	}
+		
+}
+int main(){
+	leader();
+	return 0;
+}
+
+
