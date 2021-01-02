d68839714fc4226ca9dc6d7eff2f5037961fe0bd
harshareddy794 <harshareddy794@gmail.com>
2021-01-02 06:48:49 +0530

    Added reversing a linkedlist

---
+++ Ds and algo/Basic/Linkedlist/reverse Linkedlist.cpp
@@ -0,0 +1,70 @@
+#include <iostream>
+
+using namespace std;
+
+struct Node{
+    int data;
+    Node *next;
+    Node(int x){
+        data = x;
+        next = NULL;
+    }
+};
+
+void printList(Node *head){
+    Node *curr = head;
+    while(curr!=NULL){
+        cout<<(curr->data)<<" ";
+        curr = curr->next;
+    }
+    cout<<endl;
+}
+
+Node *insertAtPosition(Node *head, int x,int position){
+    Node *temp = new Node(x);
+    if(position==1){
+        temp->next=head;
+        return temp;
+    }else{
+        int count = 1;
+        Node *curr = head;
+        while((count<position-1)&&(curr->next!=NULL)){
+            count++;
+            curr = curr->next;
+        }
+        temp->next = curr->next;
+        curr->next = temp;
+        return head;
+    }
+}
+
+//  Efficient solution
+Node *revLinkedList(Node *head){
+    if(head==NULL){
+        return head;
+    }
+    Node *prev = NULL;
+    Node *curr = head;
+    while(curr!=NULL){
+        Node *next = curr->next;
+        curr->next = prev;
+        prev = curr;
+        curr = next;
+    }
+    return prev;
+}
+
+int main()
+{   
+    Node *head =NULL;
+    head = insertAtPosition(head,10,1);
+    head = insertAtPosition(head,20,2);
+    head = insertAtPosition(head,30,3);
+    head = insertAtPosition(head,40,4);
+    head = insertAtPosition(head,45,2);
+    head = insertAtPosition(head,50,1);
+    printList(head);
+    head = revLinkedList(head);
+    printList(head);
+    return 0;
+}
