34203faeb922d38ae1145111f67c919afb791eb7
harshareddy794 <harshareddy794@gmail.com>
2021-01-17 09:26:39 +0530

    Added balanced perenthesis

---
+++ Ds and algo/Basic/Stack/parenthesesBalance.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include<stack>
+
+using namespace std;
+
+bool matching(char a, char b){
+    return(
+    (a=='(' && b==')')||
+    (a=='[' && b==']')||
+    (a=='{' && b=='}')
+    );
+}
+
+bool isBalanced(string s){
+    stack<char> a;
+    for(int i=0;i<s.length();i++){
+        if(s[i]=='(' || s[i]=='{' || s[i]=='['){
+            a.push(s[i]);
+        }else{
+            if(a.empty()==true){
+                return false;
+            }else if(matching(a.top(),s[i])==false){
+                return false;
+            }else{
+                a.pop();
+            }
+        }
+    }
+    return(a.empty());
+}
+
+int main()
+{   char a[] =  "{[()]}";
+    if(isBalanced(a)){
+        cout<<"True";
+    }else{
+        cout<<"False";
+    }
+    return 0;
+}
