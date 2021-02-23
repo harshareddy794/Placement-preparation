25a97b6ca582f46e09823100c017a121c0568521
harshareddy794 <harshareddy794@gmail.com>
2021-02-23 09:46:05 +0530

    modified kthSmallestElement

--- Ds and algo/Basic/Heap/kthSmallestElement.cpp
+++ Ds and algo/Basic/Heap/kthSmallestElement.cpp
@@ -3,22 +3,14 @@
 
 using namespace std;
 
-void smallest()
-
 int main()
 {
-    int arr[] = { 12, 3, 5, 7, 19 }, k = 2; 
+    int arr[] = { 12, 3, 5, 7, 19 }, k = 3; 
     int n = sizeof(arr)/sizeof(arr[0]);
-    priority_queue<int,vector<int>,greater<int>> pg;
-    for(int i=0;i<n;i++){
-        pg.push(arr[i]);    
-    }
+    priority_queue<int,vector<int>,greater<int>> pg(arr,arr+5);
     for(int i=1;i<k;i++){
         pg.pop();    
     }
     cout<<pg.top();
-    
-    
-   
     return 0;
 }
