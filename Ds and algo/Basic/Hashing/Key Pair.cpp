d3dc73e97a1aa58e8c867064b6ced8b98cd03cf6
harshareddy794 <harshareddy794@gmail.com>
2021-05-12 18:11:00 +0530

    added key pair problem

---
+++ Ds and algo/Basic/Hashing/Key Pair.cpp
@@ -0,0 +1,45 @@
+// { Driver Code Starts
+#include <bits/stdc++.h>
+
+using namespace std;
+
+
+ // } Driver Code Ends
+//User function template for C++
+class Solution{
+public:	
+	// Function to check if array has 2 elements
+	// whose sum is equal to the given value
+	bool hasArrayTwoCandidates(int arr[], int n, int x) {
+	    unordered_set<int> us;
+	    for(int i=0;i<n;i++){
+	        int rem = x - arr[i];
+	        if(us.find(rem)==us.end()){
+	            us.insert(arr[i]);
+	        }else{
+	            return true;
+	        }
+	    }
+	    return false;
+	}
+};
+
+// { Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n, x;
+        cin >> n >> x;
+        int arr[n];
+        for (int i = 0; i < n; i++) {
+            cin >> arr[i];
+        }
+        Solution ob;
+        auto ans = ob.hasArrayTwoCandidates(arr, n, x);
+        cout << (ans ? "Yes\n" : "No\n");
+    }
+    return 0;
+}
+  // } Driver Code Ends
